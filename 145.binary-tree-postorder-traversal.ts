a1eca52ce55ddcc72a1b214ef7e2450cbce80a41
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-14 22:53:50 +0600

    feat: Add solution for binary tree postorder traversal
    This commit introduces a TypeScript solution for LeetCode problem 145: Binary Tree Postorder Traversal. The solution uses a recursive approach to traverse the tree and return the postorder traversal of its nodes' values.

---
+++ 145.binary-tree-postorder-traversal.ts
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode id=145 lang=typescript
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.42%)
+ * Likes:    7391
+ * Dislikes: 215
+ * Total Accepted:    1.6M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ *
+ * Output: [3,2,1]
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+ *
+ * Output: [4,6,7,5,2,9,8,3,1]
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ *
+ * Output: []
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: root = [1]
+ *
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ */
+
+// class TreeNode {
+// 	val: number;
+// 	left: TreeNode | null;
+// 	right: TreeNode | null;
+// 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+// 		this.val = val === undefined ? 0 : val;
+// 		this.left = left === undefined ? null : left;
+// 		this.right = right === undefined ? null : right;
+// 	}
+// }
+
+function postorderTraversal(root: TreeNode | null): number[] {
+	const result: number[] = [];
+
+	function traversal(node: TreeNode | null) {
+		if (node === null) return;
+
+		traversal(node.left);
+		traversal(node.right);
+		result.push(node.val);
+	}
+
+	traversal(root);
+	return result;
+}
+// @lc code=end
