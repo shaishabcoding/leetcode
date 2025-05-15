20ee0b08fa48169b32880cad7c974575c888f84f
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-15 22:36:37 +0600

    feat: add solution for Power of Three (LeetCode 326)
    This commit introduces a TypeScript solution for LeetCode problem 326, \"Power of Three\". The solution uses recursion to determine if a given integer is a power of three.

---
+++ 326.power-of-three.ts
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=326 lang=typescript
+ *
+ * [326] Power of Three
+ *
+ * https://leetcode.com/problems/power-of-three/description/
+ *
+ * algorithms
+ * Easy (47.89%)
+ * Likes:    3252
+ * Dislikes: 291
+ * Total Accepted:    1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '27'
+ *
+ * Given an integer n, return true if it is a power of three. Otherwise, return
+ * false.
+ *
+ * An integer n is a power of three, if there exists an integer x such that n
+ * == 3^x.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 27
+ * Output: true
+ * Explanation: 27 = 3^3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 0
+ * Output: false
+ * Explanation: There is no x where 3^x = 0.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: n = -1
+ * Output: false
+ * Explanation: There is no x where 3^x = (-1).
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= n <= 2^31 - 1
+ *
+ *
+ *
+ * Follow up: Could you solve it without loops/recursion?
+ */
+
+// @lc code=start
+function isPowerOfThree(n: number): boolean {
+	if (n === 1) return true;
+	if (n === 0 || n % 3 !== 0) return false;
+	return isPowerOfThree(n / 3);
+}
+// @lc code=end
