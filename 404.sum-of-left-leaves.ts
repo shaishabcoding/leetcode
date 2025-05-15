23a54c0102236139f8eba0f266eb4dee9230a302
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-15 19:35:16 +0600

    feat: Add solution for Sum of Left Leaves (LeetCode 404)
    This commit adds a TypeScript solution for the LeetCode problem 404, \"Sum of Left Leaves\". The solution recursively traverses the binary tree and sums the values of all left leaves.

---
+++ 404.sum-of-left-leaves.ts
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=404 lang=typescript
+ *
+ * [404] Sum of Left Leaves
+ *
+ * https://leetcode.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (61.56%)
+ * Likes:    5628
+ * Dislikes: 315
+ * Total Accepted:    703.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return the sum of all left leaves.
+ *
+ * A leaf is a node with no children. A left leaf is a leaf that is the left
+ * child of another node.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 24
+ * Explanation: There are two left leaves in the binary tree, with values 9 and
+ * 15 respectively.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1]
+ * Output: 0
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 1000].
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ */
+
+// class TreeNode {
+// 	val: number;
+// 	left: TreeNode | null;
+// 	right: TreeNode | null;
+// 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+// 		this.val = val === undefined ? 0 : val;
+// 		this.left = left === undefined ? null : left;
+// 		this.right = right === undefined ? null : right;
+// 	}
+// }
+
+function sumOfLeftLeaves(
+	node: TreeNode | null,
+	isLeft: boolean = false
+): number {
+	if (!node) return 0;
+
+	if (!node.left && !node.right && isLeft) return node.val;
+
+	return sumOfLeftLeaves(node.left, true) + sumOfLeftLeaves(node.right, false);
+}
+// @lc code=end
