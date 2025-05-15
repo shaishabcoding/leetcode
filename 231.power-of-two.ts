1d97a9456efaabcef0f90e4b92611e73f96fd6d6
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-15 22:28:06 +0600

    Add solution for LeetCode problem 231: Power of Two
    This commit adds a TypeScript solution for the LeetCode problem 231, \"Power of Two\", which determines if a given integer is a power of two. The solution uses a recursive approach.

---
+++ 231.power-of-two.ts
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=231 lang=typescript
+ *
+ * [231] Power of Two
+ *
+ * https://leetcode.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (48.33%)
+ * Likes:    7229
+ * Dislikes: 466
+ * Total Accepted:    1.7M
+ * Total Submissions: 3.4M
+ * Testcase Example:  '1'
+ *
+ * Given an integer n, return true if it is a power of two. Otherwise, return
+ * false.
+ *
+ * An integer n is a power of two, if there exists an integer x such that n ==
+ * 2^x.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 1
+ * Output: true
+ * Explanation: 2^0 = 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 16
+ * Output: true
+ * Explanation: 2^4 = 16
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: n = 3
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= n <= 2^31 - 1
+ *
+ *
+ *
+ * Follow up: Could you solve it without loops/recursion?
+ */
+
+// @lc code=start
+function isPowerOfTwo(n: number): boolean {
+	if (n === 1) return true;
+	if (n < 1 || n % 2 !== 0) return false;
+	return isPowerOfTwo(n / 2);
+}
+// @lc code=end
