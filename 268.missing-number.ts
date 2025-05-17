2f58daaf77740007b1183922e71aadb5697f9c94
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-17 22:34:43 +0600

    feat: Add solution for LeetCode 268 - Missing Number
    This commit adds a TypeScript solution to LeetCode problem 268, \"Missing Number\". The solution utilizes bitwise XOR to find the missing number in O(n) time and O(1) space.

---
+++ 268.missing-number.ts
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=268 lang=typescript
+ *
+ * [268] Missing Number
+ *
+ * https://leetcode.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (69.80%)
+ * Likes:    13075
+ * Dislikes: 3409
+ * Total Accepted:    3M
+ * Total Submissions: 4.3M
+ * Testcase Example:  '[3,0,1]'
+ *
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [3,0,1]
+ *
+ * Output: 2
+ *
+ * Explanation:
+ *
+ * n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is
+ * the missing number in the range since it does not appear in nums.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,1]
+ *
+ * Output: 2
+ *
+ * Explanation:
+ *
+ * n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is
+ * the missing number in the range since it does not appear in nums.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [9,6,4,2,3,5,7,0,1]
+ *
+ * Output: 8
+ *
+ * Explanation:
+ *
+ * n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is
+ * the missing number in the range since it does not appear in
+ * nums.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 10^4
+ * 0 <= nums[i] <= n
+ * All the numbers of nums are unique.
+ *
+ *
+ *
+ * Follow up: Could you implement a solution using only O(1) extra space
+ * complexity and O(n) runtime complexity?
+ *
+ */
+
+// @lc code=start
+function missingNumber(nums: number[]): number {
+	let n = nums.length;
+
+	let result = n;
+
+	for (let i = 0; i < n; i++) {
+		result ^= i ^ nums[i];
+	}
+
+	return result;
+}
+// @lc code=end
