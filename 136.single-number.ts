45408bd7d480143edddea16d316230e58bd8a770
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-17 16:31:32 +0600

    feat: Add solution for LeetCode 136 - Single Number
    This commit adds a TypeScript solution for LeetCode problem 136, Single Number, which finds the single element in an array that appears only once, where all other elements appear twice. The solution utilizes the XOR bitwise operator to achieve linear runtime complexity and constant extra space.

---
+++ 136.single-number.ts
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=136 lang=typescript
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (75.74%)
+ * Likes:    17501
+ * Dislikes: 808
+ * Total Accepted:    3.6M
+ * Total Submissions: 4.8M
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers nums, every element appears twice except
+ * for one. Find that single one.
+ *
+ * You must implement a solution with a linear runtime complexity and use only
+ * constant extra space.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [2,2,1]
+ *
+ * Output: 1
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [4,1,2,1,2]
+ *
+ * Output: 4
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1]
+ *
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 3 * 10^4
+ * -3 * 10^4 <= nums[i] <= 3 * 10^4
+ * Each element in the array appears twice except for one element which appears
+ * only once.
+ *
+ *
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+	let result = 0;
+
+	for (const num of nums) {
+		result ^= num;
+	}
+
+	return result;
+}
+// @lc code=end
