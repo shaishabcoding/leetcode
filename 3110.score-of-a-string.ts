e55bee52f1b9b49d802d7ffd9f631826244bafc8
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-18 21:30:32 +0600

    feat: Implement LeetCode problem 3110 - Score of a String
    This commit adds a TypeScript solution for LeetCode problem 3110, \"Score of a String\". The solution calculates the score of a string by summing the absolute differences between the ASCII values of adjacent characters.

---
+++ 3110.score-of-a-string.ts
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=3110 lang=typescript
+ *
+ * [3110] Score of a String
+ *
+ * https://leetcode.com/problems/score-of-a-string/description/
+ *
+ * algorithms
+ * Easy (91.94%)
+ * Likes:    706
+ * Dislikes: 45
+ * Total Accepted:    352.8K
+ * Total Submissions: 383.7K
+ * Testcase Example:  '"hello"'
+ *
+ * You are given a string s. The score of a string is defined as the sum of the
+ * absolute difference between the ASCII values of adjacent characters.
+ *
+ * Return the score of s.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "hello"
+ *
+ * Output: 13
+ *
+ * Explanation:
+ *
+ * The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' =
+ * 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108
+ * - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "zaz"
+ *
+ * Output: 50
+ *
+ * Explanation:
+ *
+ * The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the
+ * score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 2 <= s.length <= 100
+ * s consists only of lowercase English letters.
+ *
+ *
+ */
+
+// @lc code=start
+function scoreOfString(s: string): number {
+	let score = 0;
+	for (let i = 0; i < s.length - 1; i++) {
+		score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+	}
+
+	return score;
+}
+// @lc code=end
