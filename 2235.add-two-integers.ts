075402aee4c3007596239646a7c01ca48cc1bb0d
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-18 22:01:08 +0600

    feat: Add solution for LeetCode problem 2235. Add Two Integers
    This commit introduces a TypeScript solution for LeetCode problem 2235, \"Add Two Integers\". The function `sum` takes two numbers as input and returns their sum.

---
+++ 2235.add-two-integers.ts
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=2235 lang=typescript
+ *
+ * [2235] Add Two Integers
+ *
+ * https://leetcode.com/problems/add-two-integers/description/
+ *
+ * algorithms
+ * Easy (88.18%)
+ * Likes:    1813
+ * Dislikes: 3178
+ * Total Accepted:    694.9K
+ * Total Submissions: 788K
+ * Testcase Example:  '12\n5'
+ *
+ * Given two integers num1 and num2, return the sum of the two integers.
+ *
+ * Example 1:
+ *
+ *
+ * Input: num1 = 12, num2 = 5
+ * Output: 17
+ * Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is
+ * returned.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: num1 = -10, num2 = 4
+ * Output: -6
+ * Explanation: num1 + num2 = -6, so -6 is returned.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -100 <= num1, num2 <= 100
+ *
+ *
+ */
+
+// @lc code=start
+function sum(num1: number, num2: number): number {
+	return num1 + num2;
+}
+// @lc code=end
