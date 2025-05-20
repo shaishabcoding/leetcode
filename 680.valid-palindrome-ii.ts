6264766656b04f635908c26cb68925c21dfcdc39
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-20 22:53:03 +0600

    feat: Add solution for Valid Palindrome II (680)
    This commit adds a TypeScript solution for LeetCode problem 680, Valid Palindrome II. The solution checks if a string can become a palindrome by removing at most one character.

---
+++ 680.valid-palindrome-ii.ts
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=680 lang=typescript
+ *
+ * [680] Valid Palindrome II
+ *
+ * https://leetcode.com/problems/valid-palindrome-ii/description/
+ *
+ * algorithms
+ * Easy (42.86%)
+ * Likes:    8575
+ * Dislikes: 482
+ * Total Accepted:    959.1K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '"aba"'
+ *
+ * Given a string s, return true if the s can be palindrome after deleting at
+ * most one character from it.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "aba"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "abca"
+ * Output: true
+ * Explanation: You could delete the character 'c'.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "abc"
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^5
+ * s consists of lowercase English letters.
+ *
+ *
+ */
+
+// @lc code=start
+function validPalindrome(s: string): boolean {
+	let left = 0;
+	let right = s.length - 1;
+
+	function isPalindrome(s: string, left: number, right: number): boolean {
+		while (left < right) {
+			if (s[left] !== s[right]) return false;
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+
+	while (left < right) {
+		if (s[left] !== s[right]) {
+			return (
+				isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+			);
+		}
+		left++;
+		right--;
+	}
+
+	return true;
+}
+// @lc code=end
