a4a07909b424d0ec08fad88b186fc955b0f68f7e
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-21 22:09:03 +0600

    feat: Add solution for Valid Parentheses (LeetCode #20)
    This commit introduces a TypeScript solution for the LeetCode problem #20, \"Valid Parentheses\". The solution utilizes a stack to verify the correct nesting and matching of parentheses.

---
+++ 20.valid-parentheses.ts
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=20 lang=typescript
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (42.17%)
+ * Likes:    25714
+ * Dislikes: 1875
+ * Total Accepted:    6M
+ * Total Submissions: 14.3M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "()"
+ *
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "()[]{}"
+ *
+ * Output: true
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "(]"
+ *
+ * Output: false
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: s = "([])"
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^4
+ * s consists of parentheses only '()[]{}'.
+ *
+ *
+ */
+
+// @lc code=start
+function isValid(s: string): boolean {
+	const stack: string[] = [];
+	const map = {
+		")": "(",
+		"}": "{",
+		"]": "[",
+	};
+
+	for (const char of s) {
+		if (char in map) {
+			const topElement = stack.length === 0 ? "" : stack.pop();
+			if (topElement !== map[char]) {
+				return false;
+			}
+		} else {
+			stack.push(char);
+		}
+	}
+
+	return stack.length === 0;
+}
+// @lc code=end
