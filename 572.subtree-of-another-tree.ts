2e63ecad4754251ad33d2864825d5455c3adfb98
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-21 18:56:40 +0600

    feat: Add solution for Subtree of Another Tree (LeetCode 572)

---
+++ 572.subtree-of-another-tree.ts
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=572 lang=typescript
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (49.79%)
+ * Likes:    8547
+ * Dislikes: 564
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * Given the roots of two binary trees root and subRoot, return true if there
+ * is a subtree of root with the same structure and node values of subRoot and
+ * false otherwise.
+ *
+ * A subtree of a binary tree tree is a tree that consists of a node in tree
+ * and all of this node's descendants. The tree tree could also be considered
+ * as a subtree of itself.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the root tree is in the range [1, 2000].
+ * The number of nodes in the subRoot tree is in the range [1, 1000].
+ * -10^4 <= root.val <= 10^4
+ * -10^4 <= subRoot.val <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ */
+
+// class TreeNode {
+// 	val: number;
+// 	left: TreeNode | null;
+// 	right: TreeNode | null;
+// 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+// 		this.val = val === undefined ? 0 : val;
+// 		this.left = left === undefined ? null : left;
+// 		this.right = right === undefined ? null : right;
+// 	}
+// }
+
+function isSameTree(root: TreeNode | null, target: TreeNode | null): boolean {
+	if (!root && !target) return true;
+	if (!root || !target) return false;
+	if (root.val !== target.val) return false;
+
+	return (
+		isSameTree(root.left, target.left) && isSameTree(root.right, target.right)
+	);
+}
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+	if (!subRoot) return true;
+	if (!root) return false;
+
+	if (isSameTree(root, subRoot)) return true;
+
+	return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+// @lc code=end
