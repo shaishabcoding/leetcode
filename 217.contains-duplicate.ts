9dc4a4bffdb52509ed59691e6c92c992102b382e
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-21 23:03:37 +0600

    feat: Add solution for LeetCode problem 217 (Contains Duplicate)
    This commit introduces a TypeScript solution to the LeetCode problem 217, which determines if an array contains any duplicate values. It utilizes a Map to efficiently check for duplicates.

---
+++ 217.contains-duplicate.ts
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (63.09%)
+ * Likes:    12853
+ * Dislikes: 1328
+ * Total Accepted:    5.1M
+ * Total Submissions: 8.1M
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [1,2,3,1]
+ *
+ * Output: true
+ *
+ * Explanation:
+ *
+ * The element 1 occurs at the indices 0 and 3.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [1,2,3,4]
+ *
+ * Output: false
+ *
+ * Explanation:
+ *
+ * All elements are distinct.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ *
+ *
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+	const map = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		if (map.has(nums[i])) {
+			return true;
+		} else {
+			map.set(nums[i], i);
+		}
+	}
+
+	return false;
+}
+// @lc code=end
