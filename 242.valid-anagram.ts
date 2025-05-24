2e21e064891bbe815e8da65a5052dd4b37dd0a6d
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-24 20:14:50 +0600

    feat: Optimize valid anagram solution
    This commit optimizes the valid anagram solution by using a single array to track character frequencies instead of two separate objects, improving performance.

--- 242.valid-anagram.ts
+++ 242.valid-anagram.ts
@@ -51,21 +51,15 @@
 function isAnagram(s: string, t: string): boolean {
 	if (s.length !== t.length) return false;
 
-	const freq1 = {};
-	const freq2 = {};
+	const count = new Array(26).fill(0);
 
 	for (let i = 0; i < s.length; i++) {
-		if (freq1[s[i]]) freq1[s[i]]++;
-		else freq1[s[i]] = 1;
+		count[s.charCodeAt(i) - 97]++;
+		count[t.charCodeAt(i) - 97]--;
 	}
 
-	for (let i = 0; i < t.length; i++) {
-		if (freq2[t[i]]) freq2[t[i]]++;
-		else freq2[t[i]] = 1;
-	}
-
-	for (let key in freq1) {
-		if (freq1[key] !== freq2[key]) return false;
+	for (const c of count) {
+		if (c !== 0) return false;
 	}
 
 	return true;
