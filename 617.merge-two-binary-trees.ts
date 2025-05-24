9be2a73793f742045dac07c351d86cfddbab28d1
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-24 17:55:21 +0600

    Refactor: Use TreeNode class directly and rename root variables for clarity
    The TreeNode class was uncommented and used directly. Renamed variables \"root1\" and \"root2\" to \"tree1\" and \"tree2\" to improve code readability.

--- 617.merge-two-binary-trees.ts
+++ 617.merge-two-binary-trees.ts
@@ -50,33 +50,33 @@
  *
  */
 
-// @lc code=start
 /**
  * Definition for a binary tree node.
  */
 
-// class TreeNode {
-// 	val: number;
-// 	left: TreeNode | null;
-// 	right: TreeNode | null;
-// 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-// 		this.val = val === undefined ? 0 : val;
-// 		this.left = left === undefined ? null : left;
-// 		this.right = right === undefined ? null : right;
-// 	}
-// }
+class TreeNode {
+	val: number;
+	left: TreeNode | null;
+	right: TreeNode | null;
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.left = left === undefined ? null : left;
+		this.right = right === undefined ? null : right;
+	}
+}
 
+// @lc code=start
 function mergeTrees(
-	root1: TreeNode | null,
-	root2: TreeNode | null
+	tree1: TreeNode | null,
+	tree2: TreeNode | null
 ): TreeNode | null {
-	if (!root1) return root2;
-	if (!root2) return root1;
+	if (!tree1) return tree2;
+	if (!tree2) return tree1;
 
-	const merged = new TreeNode(root1.val + root2.val);
-	merged.left = mergeTrees(root1.left, root2.left);
-	merged.right = mergeTrees(root1.right, root2.right);
+	const mergedTree = new TreeNode(tree1.val + tree2.val);
+	mergedTree.left = mergeTrees(tree1.left, tree2.left);
+	mergedTree.right = mergeTrees(tree1.right, tree2.right);
 
-	return merged;
+	return mergedTree;
 }
 // @lc code=end
