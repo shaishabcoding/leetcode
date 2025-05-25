bbed609a381e7a09346f7b69aefacaa3023aaed7
shaishabcoding <shaishabchandrashil@gmail.com>
2025-05-25 15:31:03 +0600

    feat: Implement symmetric tree solution
    This commit implements the solution for checking if a binary tree is symmetric using a recursive approach. It includes the TreeNode definition and the isSymmetric function with a helper function isMirror.

--- 101.symmetric-tree.ts
+++ 101.symmetric-tree.ts
@@ -43,32 +43,33 @@
  * Follow up: Could you solve it both recursively and iteratively?
  */
 
-// @lc code=start
 /**
  * Definition for a binary tree node.
  */
 
-// class TreeNode {
-// 	val: number;
-// 	left: TreeNode | null;
-// 	right: TreeNode | null;
-// 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-// 		this.val = val === undefined ? 0 : val;
-// 		this.left = left === undefined ? null : left;
-// 		this.right = right === undefined ? null : right;
-// 	}
-// }
+class TreeNode {
+	val: number;
+	left: TreeNode | null;
+	right: TreeNode | null;
+	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.left = left === undefined ? null : left;
+		this.right = right === undefined ? null : right;
+	}
+}
 
+// @lc code=start
 function isSymmetric(root: TreeNode | null): boolean {
 	if (!root) return true;
 
-	function isMirror(t1: TreeNode | null, t2: TreeNode | null): boolean {
-		if (!t1 && !t2) return true;
-		if (!t1 || !t2) return false;
+	function isMirror(tree1: TreeNode | null, tree2: TreeNode | null): boolean {
+		if (!tree1 && !tree2) return true;
+		if (!tree1 || !tree2) return false;
+
 		return (
-			t1.val === t2.val &&
-			isMirror(t1.left, t2.right) &&
-			isMirror(t1.right, t2.left)
+			tree1.val === tree2.val &&
+			isMirror(tree1.left, tree2.right) &&
+			isMirror(tree1.right, tree2.left)
 		);
 	}
 
