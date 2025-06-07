eb8ac9eced7e60b9ae47b9ba57cd1a9a72f3df51
shaishabcoding <shaishabchandrashil@gmail.com>
2025-06-07 21:21:38 +0600

    Refactor: Move backtrack function inside permute function
    This commit refactors the code by moving the backtrack function inside the permute function. This improves code organization and encapsulation.

--- 46.permutations.ts
+++ 46.permutations.ts
@@ -2,23 +2,21 @@
 function permute(nums: number[]): number[][] {
 	const result: number[][] = [];
 
-	backTrack(nums, 0, result);
+	function backTrack(start: number) {
+		if (start === nums.length) return result.push([...nums]);
+
+		for (let i = start; i < nums.length; i++) {
+			[nums[i], nums[start]] = [nums[start], nums[i]];
+			backTrack(start + 1);
+			[nums[i], nums[start]] = [nums[start], nums[i]];
+		}
+	}
+
+	backTrack(0);
 
 	return result;
 }
 
-function backTrack(nums: number[], start: number, result: number[][]) {
-	if (start === nums.length) {
-		result.push([...nums]);
-		return;
-	}
-
-	for (let i = start; i < nums.length; i++) {
-		[nums[i], nums[start]] = [nums[start], nums[i]];
-		backTrack(nums, start + 1, result);
-		[nums[i], nums[start]] = [nums[start], nums[i]];
-	}
-}
 // @lc code=
 
 console.log(permute([1, 2, 3])); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
