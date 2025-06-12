ddff707f12a2f8f18416d13a5aa9da54561c5dee
shaishabcoding <shaishabchandrashil@gmail.com>
2025-06-12 20:23:36 +0600

    feat: solve leetcode problem 62 unique paths
    This commit adds a solution to LeetCode problem 62, Unique Paths, using a combinatorial approach.

---
+++ 62.unique-paths.ts
@@ -0,0 +1,72 @@
+// @lc code=start
+function uniquePaths(m: number, n: number): number {
+	let totalSteps = m + n - 1;
+	let k = m < n ? m : n;
+	let result = 1;
+
+	for (let i = 1; i < k; i++) result = (result * (totalSteps - k + i)) / i;
+
+	return result;
+}
+// @lc code=end
+
+console.log(uniquePaths(3, 7)); // 28
+console.log(uniquePaths(3, 2)); // 3
+console.log(uniquePaths(1, 1)); // 1
+console.log(uniquePaths(1, 2)); // 1
+console.log(uniquePaths(2, 1)); // 1
+
+/*
+ * @lc app=leetcode id=62 lang=typescript
+ *
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (65.64%)
+ * Likes:    17515
+ * Dislikes: 470
+ * Total Accepted:    2.3M
+ * Total Submissions: 3.6M
+ * Testcase Example:  '3\n7'
+ *
+ * There is a robot on an m x n grid. The robot is initially located at the
+ * top-left corner (i.e., grid[0][0]). The robot tries to move to the
+ * bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move
+ * either down or right at any point in time.
+ *
+ * Given the two integers m and n, return the number of possible unique paths
+ * that the robot can take to reach the bottom-right corner.
+ *
+ * The test cases are generated so that the answer will be less than or equal
+ * to 2 * 10^9.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: m = 3, n = 7
+ * Output: 28
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation: From the top-left corner, there are a total of 3 ways to reach
+ * the bottom-right corner:
+ * 1. Right -> Down -> Down
+ * 2. Down -> Down -> Right
+ * 3. Down -> Right -> Down
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= m, n <= 100
+ *
+ *
+ */
