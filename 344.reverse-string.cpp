89e28da8be33a6551f0041ddc485034f717bb1fa
shaishabcoding <shaishabchandrashil@gmail.com>
2025-08-03 11:11:03 +0600

    feat: Implement solution for LeetCode problem 344 (Reverse String)

---
+++ 344.reverse-string.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        reverse(s.begin(), s.end());
+    }
+};
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=344 lang=cpp
+ *
+ * [344] Reverse String
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ *
+ * algorithms
+ * Easy (79.86%)
+ * Likes:    9109
+ * Dislikes: 1202
+ * Total Accepted:    3.3M
+ * Total Submissions: 4.1M
+ * Testcase Example:  '["h","e","l","l","o"]'
+ *
+ * Write a function that reverses a string. The input string is given as an
+ * array of characters s.
+ *
+ * You must do this by modifying the input array in-place with O(1) extra
+ * memory.
+ *
+ *
+ * Example 1:
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^5
+ * s[i] is a printable ascii character.
+ *
+ *
+ */
