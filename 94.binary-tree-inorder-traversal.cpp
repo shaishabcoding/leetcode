4e0e3e64e01a58c4d2ffb51f4d374fc4c48025dd
shaishabcoding <shaishabchandrashil@gmail.com>
2025-08-04 08:38:43 +0600

    feat: Add solution for Binary Tree Inorder Traversal (recursive)
    This commit adds a C++ solution to LeetCode problem 94, Binary Tree Inorder Traversal, using a recursive approach.

---
+++ 94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+
+        traverse(root, result);
+
+        return result;
+    }
+
+    void traverse(TreeNode *node, vector<int> &result)
+    {
+        if (!node)
+            return;
+
+        traverse(node->left, result);  // left
+        result.push_back(node->val);   // current
+        traverse(node->right, result); // right
+    }
+};
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=94 lang=cpp
+ *
+ * [94] Binary Tree Inorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (78.75%)
+ * Likes:    14321
+ * Dislikes: 854
+ * Total Accepted:    3.2M
+ * Total Submissions: 4.1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ *
+ * Output: [1,3,2]
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+ *
+ * Output: [4,2,6,5,7,1,3,9,8]
+ *
+ * Explanation:
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ *
+ * Output: []
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: root = [1]
+ *
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
